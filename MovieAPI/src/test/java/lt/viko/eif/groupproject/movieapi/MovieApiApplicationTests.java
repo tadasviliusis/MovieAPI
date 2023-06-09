3e64f501aa0a3034f42aa528629faf2a84781aa8
tadasviliusis <90383964+tadasviliusis@users.noreply.github.com>
2023-06-09 18:57:44 +0300

    Add files via upload

---
+++ MovieAPI/src/test/java/lt/viko/eif/groupproject/movieapi/MovieApiApplicationTests.java
@@ -0,0 +1,13 @@
+package lt.viko.eif.groupproject.movieapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
