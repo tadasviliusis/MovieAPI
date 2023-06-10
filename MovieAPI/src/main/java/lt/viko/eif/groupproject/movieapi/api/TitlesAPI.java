dfc78f4baa5c2f30dc136316b8dcf94ace81553e
edgarsem <90824295+edgarsem@users.noreply.github.com>
2023-06-10 18:41:56 +0300

    Add files via upload

--- MovieAPI/src/main/java/lt/viko/eif/groupproject/movieapi/api/TitlesAPI.java
+++ MovieAPI/src/main/java/lt/viko/eif/groupproject/movieapi/api/TitlesAPI.java
@@ -26,7 +26,6 @@ public class TitlesAPI {
         }
     }
 
-
     public static String getTitleRating(String id) throws IOException {
         OkHttpClient client = new OkHttpClient();
 
@@ -35,6 +34,22 @@ public class TitlesAPI {
                 .get()
                 .addHeader("x-rapidapi-key", API_KEY)
                 .addHeader("x-rapidapi-host", HOST)
+                .build();
+
+        try (Response response = client.newCall(request).execute()) {
+            String json = response.body().string();
+            return json;
+        }
+    }
+
+    public static String getTitleActors(String id) throws IOException {
+        OkHttpClient client = new OkHttpClient();
+
+        Request request = new Request.Builder()
+                .url("https://moviesminidatabase.p.rapidapi.com/movie/id/" + id + "/cast/")
+                .get()
+                .addHeader("x-rapidapi-key", API_KEY)
+                .addHeader("x-rapidapi-host", "moviesminidatabase.p.rapidapi.com")
                 .build();
 
         try (Response response = client.newCall(request).execute()) {
