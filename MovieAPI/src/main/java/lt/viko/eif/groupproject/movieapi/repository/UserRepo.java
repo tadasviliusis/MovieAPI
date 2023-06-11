8adeefec1859cf9850962622b550b9d43a374c23
edgarsem <90824295+edgarsem@users.noreply.github.com>
2023-06-11 19:59:11 +0300

    Add files via upload

--- MovieAPI/src/main/java/lt/viko/eif/groupproject/movieapi/repository/UserRepo.java
+++ MovieAPI/src/main/java/lt/viko/eif/groupproject/movieapi/repository/UserRepo.java
@@ -1,7 +1,20 @@
 package lt.viko.eif.groupproject.movieapi.repository;
 
+import jakarta.transaction.Transactional;
 import lt.viko.eif.groupproject.movieapi.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface UserRepo extends JpaRepository<User, Long> {
+    @Modifying
+    @Transactional
+    @Query(value = "DELETE FROM User WHERE id = :Id", nativeQuery = true)
+    void deleteUserById(@Param("Id") Long userId);
+
+    @Modifying
+    @Transactional
+    @Query(value = "UPDATE User u SET u.username = :newUsername, u.password = :newPassword WHERE u.id = :userId", nativeQuery = true)
+    void updateUserById(@Param("newUsername") String newUsername, @Param("newPassword") String newPassword, @Param("userId") Long userId);
 }
--- MovieAPI/src/main/java/lt/viko/eif/groupproject/movieapi/repository/WatchlistRepo.java
+++ MovieAPI/src/main/java/lt/viko/eif/groupproject/movieapi/repository/WatchlistRepo.java
@@ -8,6 +8,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Map;
 
 public interface WatchlistRepo extends JpaRepository<Watchlist, Long> {
     @Query("FROM Watchlist g where g.user.id = :userId")
